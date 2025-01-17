74b48e78439291accc844dc7241bbd2ecabd321b
Venkatesh <venkateshkasani14@gmail.com>
2025-01-17 08:53:04 +0530

    type fixes -2

--- frontend/src/custom-components/Searchbar.tsx
+++ frontend/src/custom-components/Searchbar.tsx
@@ -60,7 +60,7 @@ const Searchbar = () => {
                     <X onClick={() => setSearch('')} className={clsx('text-gray-500 absolute right-1 hover:cursor-pointer',{'block':search.length > 0,'hidden':search.length < 1})} size={20} />
             </div>
             <div className={clsx("absolute max-h-[50vh] overflow-y-auto top-10 z-10 w-full rounded mt-1 bg-slate-200 text-black font-semibold flex flex-col gap-2",{'hidden':!open})}>
-                {searchResults?.map((movie:movieEditType|any,index:number) => (
+                {searchResults?.map((movie:movieEditType,index:number) => (
                     <Link key={index} href={`/movies/${movie?._id}`} className="hover:bg-gray-300">
                     <span onClick={() => console.log("selected",movie?.movieName??'')} className="p-2">
                        {movie.movieName}
